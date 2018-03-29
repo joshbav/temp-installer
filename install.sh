2d103af3f21f1075d32b5507d6b3e49121019ff4
“joshbav” <josh@Joshs-Mac.home>
2018-03-29 16:28:27 -0700

    scripted commit

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo yum update -y 
 sudo yum install -y epel-release
-sudo yum install -y tar xz unzip curl ipset bind-utils autofs nano ftp jq wget expect net-tools traceroute iproute telnet unzip python34-setuptools python34-pip
+sudo yum install -y tar xz unzip curl ipset bind-utils autofs nano ftp jq wget expect net-tools traceroute iproute telnet unzip python34-setuptools python34-pip yum-utils device-mapper-persistent-data lvm2
 sudo pip3 install --upgrade pip
 sudo pip3 install virtualenv 
 
@@ -13,13 +13,13 @@ sudo systemctl stop firewalld
 sudo systemctl disable firewalld
 
 
-#####Verify NTP is setup and synchronized (not necessary in AWS)
+##### Verify NTP is setup and synchronized (not necessary in AWS)
 timedatectl 
 
-#Disable SELinux
+#### Disable SELinux
 sudo sed -i s/SELINUX=enforcing/SELINUX=permissive/g /etc/selinux/config
 
-# Verify we boot to the GUI 
+#### Verify we boot to the GUI 
 #Dont do this on the bootstrap node because of disney license server
 systemctl set-default multi-user.target
 
@@ -28,23 +28,25 @@ systemctl set-default multi-user.target
 # use localectl to check
 
 #### DOCKER
+
 sudo tee /etc/modules-load.d/overlay.conf <<-'EOF'
 overlay
 EOF
 
-sudo yum -y install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager  --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum-config-manager --enable docker-ce-edge
-sudo yum-config-manager --enable docker-ce-test
-yum remove -y docker-engine-selinux container-selinux
-yum install -y --setopt=obsoletes=0 docker-ce-17.05.0.ce
+sudo yum install -y ddocker-ce-17.12.1.ce
+
+#sudo yum-config-manager --enable docker-ce-edge
+#sudo yum-config-manager --enable docker-ce-test
+#sudo yum remove -y docker-engine-selinux container-selinux
+#sudo yum install -y --setopt=obsoletes=0 docker-ce-17.05.0.ce
 
 #Configure systemd to run the Docker Daemon with OverlayFS
-sudo mkdir -p /etc/systemd/system/docker.service.d && sudo tee /etc/systemd/system/docker.service.d/override.conf <<- EOF
-[Service]
-ExecStart=
-ExecStart=/usr/bin/dockerd --storage-driver=overlay
-EOF
+#sudo mkdir -p /etc/systemd/system/docker.service.d && sudo tee /etc/systemd/system/docker.service.d/override.conf <<- EOF
+#[Service]
+#ExecStart=
+#ExecStart=/usr/bin/dockerd --storage-driver=overlay
+#EOF
 
 sudo systemctl enable docker
 
@@ -57,6 +59,7 @@ sudo groupadd nogroup && sudo groupadd docker
 systemctl disable libvirtd.service
 
 # done
+echo
 echo
 echo DONE. Please reboot then do a docker run hello-world   to test
 
